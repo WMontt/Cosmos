c00c81b8ae7ee7411968788858c26cffece1ad38
WMontt <wmonthant11@gmail.com>
2025-03-24 22:44:31 +0630

    Added more html. css and functions

--- src/index.js
+++ src/index.js
@@ -2,14 +2,71 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let timeElement = document.querySelector("#time");
+  let dayElement = document.querySelector("#day");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  timeElement.innerHTML = formatTime(date);
+  dayElement.innerHTML = formatDate(date);
+}
+
+function formatTime(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 12) {
+    return `${hours}:${minutes} AM`;
+  } else {
+    hours = hours - 12;
+    return `${hours}:${minutes} PM`;
+  }
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let today = date.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  return `${day}, ${today}  ${month} ${year}`;
 }
 
 function searchCity(city) {
   let apiKey = "34eba43b83b3fa388doc023be8fb7t33";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(refreshWeather);
 }
 
